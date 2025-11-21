d62b9db0dbf8f34bcef4dae65731b6861a8af720
Владимир Арутюнов <vladimir.arutyunov@t2.ru>
2025-11-21 10:10:48 +0400

    feat(trending-section): add movies and series sections to trending

--- components/trending-section.tsx
+++ components/trending-section.tsx
@@ -26,6 +26,16 @@ const TRENDING_SECTIONS: TrendingItem[] = [
     title: "Обновления",
     playlist_url: "https://api.vokino.pro/v2/list?sort=updatings",
   },
+  {
+    title: "Фильмы",
+    playlist_url:
+      "https://api.vokino.pro/v2/list?sort=popular&type=movie&page=1&token=mac_23602515ddd41e2f1a3eba4d4c8a949a_1225352",
+  },
+  {
+    title: "Сериалы",
+    playlist_url:
+      "https://api.vokino.pro/v2/list?sort=popular&type=serial&page=1&token=mac_23602515ddd41e2f1a3eba4d4c8a949a_1225352",
+  },
 ]
 
 type TrendingSectionProps = {
